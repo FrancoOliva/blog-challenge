2b62662ce94aa675185c898d9aa721b875a0163a
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-20 12:36:06 -0300

    Home Page

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
+import { MyprofileComponent } from './pages/myprofile/myprofile.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MyprofileComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     FeaturesRoutingModule
