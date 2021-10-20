0f81877b8df9bff7c78a3fa819fd96789b24cc9a
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-20 13:56:46 -0300

    HttpClient, user interface

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MyprofileComponent } from './pages/myprofile/myprofile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'mi-perfil',
+    component: MyprofileComponent
   }
 ];
 
--- src/app/features/pages/myprofile/myprofile.component.ts
+++ src/app/features/pages/myprofile/myprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonPlaceholderService } from '../../../core/services/json-placeholder.service';
 
 @Component({
   selector: 'app-myprofile',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyprofileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jsonService: JsonPlaceholderService) { }
 
   ngOnInit(): void {
+
+    this.jsonService.getUser().subscribe( console.log );
+    
   }
 
 }
