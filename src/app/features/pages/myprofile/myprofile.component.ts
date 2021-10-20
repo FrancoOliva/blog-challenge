b0e8d3b4a798dc060cb08f857a6c528e64e5d8d3
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-20 16:14:20 -0300

    url avatar

--- src/app/features/pages/myprofile/myprofile.component.ts
+++ src/app/features/pages/myprofile/myprofile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/core/interfaces/user.interface';
 import { JsonPlaceholderService } from '../../../core/services/json-placeholder.service';
+
 
 @Component({
   selector: 'app-myprofile',
@@ -8,12 +10,34 @@ import { JsonPlaceholderService } from '../../../core/services/json-placeholder.
 })
 export class MyprofileComponent implements OnInit {
 
+  user!: User;
+  mostrar: boolean = false;
+
   constructor(private jsonService: JsonPlaceholderService) { }
 
   ngOnInit(): void {
 
-    this.jsonService.getUser().subscribe( console.log );
-    
+    this.jsonService.getUser().subscribe( user => {
+      
+      this.user = user;
+
+      this.mostrar = true;
+
+      localStorage.setItem('user', JSON.stringify(this.user) );
+    });
+
+  }
+
+  posts(){
+    console.log('mostrar todos los posts');
+  }
+
+  albums(){
+    console.log('mostrar todos los albums');
+  }
+
+  todos(){
+    console.log('mostrar todos los to dos');
   }
 
 }
